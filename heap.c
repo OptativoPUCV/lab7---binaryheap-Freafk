551312da13e3ba8bfd1887406a86b9995e79c866
Replit user <>
2023-05-12 16:54:00 +0000

    new try

--- heap.c
+++ heap.c
@@ -19,15 +19,43 @@ typedef struct Heap{
 
 void* heap_top(Heap* pq){
   
-  //heapElem *aux=&pq->heapArray[0];
   if(pq->size==0)return NULL;
   return pq->heapArray[0].data;
 }
 
 
 
-void heap_push(Heap* pq, void* data, int priority){
 
+
+void upHeap(Heap *pq,int index){
+  int indexPadre;
+  if(index%2==0){
+    indexPadre=index/2;
+  }
+  else{
+    indexPadre=(index-1)/2;
+  }
+  if(pq->heapArray[index].priority>pq->heapArray[indexPadre].priority){
+    heapElem aux=pq->heapArray[indexPadre];
+    pq->heapArray[indexPadre]=pq->heapArray[index];
+    pq->heapArray[index]=aux;
+    upHeap(pq,indexPadre);
+  }
+}
+
+
+
+
+
+void heap_push(Heap* pq, void* data, int priority){
+  heapElem node={data,priority};
+  if(pq->size==pq->capac){
+    pq->heapArray=realloc(pq->heapArray,sizeof(pq->heapArray)*2);
+  }
+  int index=pq->size;
+  pq->heapArray[index]=node;
+  pq->size++;
+  upHeap(pq,index);
 }
 
 
