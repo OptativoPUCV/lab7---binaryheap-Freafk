bd38473f71a2ca48f5602b52350f6e949e902337
Replit user <>
2023-05-12 17:12:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -33,7 +33,7 @@ void upHeap(Heap *pq,int index){
   if(index<=2){
     indexPadre=0;
   }
-  if(index%2==0){
+  else if(index%2==0){
     indexPadre=index/2;
   }
   else{
@@ -52,6 +52,9 @@ void upHeap(Heap *pq,int index){
 
 
 void heap_push(Heap* pq, void* data, int priority){
+  for(int i=0;i<pq->size;i++){
+    printf("%d, ",pq->heapArray[i].priority);
+  }
   heapElem node={data,priority};
   if(pq->size==pq->capac-2){
     pq->heapArray=realloc(pq->heapArray,sizeof(heapElem)*(pq->capac*2));
